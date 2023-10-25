8035f35802acefba7236218530176488ed3d0661
HashemNAVA77 <143622962+HashemNAVA77@users.noreply.github.com>
2023-10-25 09:03:56 -0600

    Add files via upload

---
+++ EXAMUNID3HNB.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examunid3hnb;
+
+/**
+ *
+ * @author Usuario
+ */
+Scanner Scanner = new Scanner(System.in);
+
+public class EXAMUNID3HNB {
+
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDOS A LA CAFETERIA");
+        String CAPPUCCCINO = null;
+        String LATTE = null;
+        String MATCHA = null;
+        System.out.print(CAPPUCCCINO);
+        System.out.println(LATTE);
+        System.out.println(MATCHA);  
+    }
+    
+             case  1  {
+    CAPPUCCINO "CH = 25.30";
+    CAPPUCCINO "M = 30.0";
+    CAPPUCCINO "G = 37.5";
+}
+   defaul
+         
+            case 2 {
+    LATTE "CH = 32.5";
+    LATTE "M = 38.4";
+    LATTE "G = 42.5";   
+}
+        defaul
+                    case 3{
+     MATCHA "CH = 41.5";
+     MATCHA "M = 48.9";
+     MATCHA "G = 52.5";
+} 
+    
+}
